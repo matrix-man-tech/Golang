a234e47e40c4e35036dc1f0f7bc37bea5ec1ff8d
rahul <rahulparakkal98@gmail.com>
2025-08-10 19:03:18 +0530

    done

--- mygolang-lco/02variables/main.go
+++ mygolang-lco/02variables/main.go
@@ -6,4 +6,17 @@ func main() {
 	var username string = "Rahul"
 	fmt.Println(username)
 	fmt.Printf("the type of the variable is : %T \n", username)
+
+	var isLoggedIn bool = true
+	fmt.Println(isLoggedIn)
+	fmt.Printf("the type of the variable is : %T \n", isLoggedIn)
+
+	var smallVal uint8 = 255
+	fmt.Println(smallVal)
+	fmt.Printf("the type of the variable is : %T \n", smallVal)
+
+	var smallFloat float32 = 245.3456
+	fmt.Println(smallFloat)
+	fmt.Printf("the type of the variable is : %T \n", smallFloat)
+
 }
